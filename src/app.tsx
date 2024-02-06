278b49f72c2768fc82666b71b70e2f07f4d74446
JoaoArouca <jarouca21@gmail.com>
2024-02-06 19:39:53 -0300

    feat: create dashboard notes page

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,25 @@
+import logo from './assets/logo.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
 export function App() {
-  return <h1>hello world</h1>
+  return (
+    <main className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="logo" />
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Find your notes..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+        />
+      </form>
+      <div className="h-px bg-slate-700" />
+      <section className="grid grid-cols-3 auto-rows-[250px] gap-6">
+        <NewNoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+      </section>
+    </main>
+  )
 }
